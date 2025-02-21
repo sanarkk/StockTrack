7487e125510b6099a3edc649a3e67261cb2e7e0b
Oleksandr Dimbrovskyi <sanya.dimbrovskyi@gmail.com>
2025-02-21 17:16:12 -0600

    Cors error fix

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,20 @@
 import asyncio
 from uvicorn import run
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from api.user_routes import router as user_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user_router)
