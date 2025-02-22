e28f76952cba215da3f10f87ecb9b592d0e939cb
Toni Odujinrin <todujinrin@gmail.com>
2025-02-22 08:54:37 -0600

    added stock data

--- frontend/src/components/HomePage/HomePage.jsx
+++ frontend/src/components/HomePage/HomePage.jsx
@@ -22,13 +22,22 @@ const HomePage = () => {
     const [search, set_search] = useState("");
     const [stocks, set_stocks] = useState([]);
     const {getSuggestions, set_selected_stocks, selected_stocks} = useContext(StocksContext)
-    const {articles, getArticles} = useContext(ArticleContext)
+    const {articles, getArticles, getStockData} = useContext(ArticleContext)
+    const [display_article, set_display_article] = useState(articles[0])
+    
     useEffect(() => {
         getSuggestions(search, set_stocks)
     }, [search]);
     useEffect(()=>{
         getArticles()
     },[])
+    useEffect(()=>{
+        set_display_article(articles[0])
+    },[articles])
+    useEffect(()=>{
+        getStockData(display_article.stock_ticker); 
+    },[display_article])
+    
     return (
         <div className={styles.wrapper}>
             <div className={styles.sidebar}>
@@ -76,7 +85,7 @@ const HomePage = () => {
                     {
                         
                         articles.map((article)=>( 
-                            <div className={styles['article-card']}>
+                            <div onClick={()=>{set_display_article(article)}} className={styles['article-card']}>
                                 <div className={styles['stock-info']}>
                                     <p className={styles['stock-short-name']}>
                                        {article.stock_ticker}
@@ -109,60 +118,12 @@ const HomePage = () => {
 }
 </div>
                     <div className={styles['stock-article']}>
+                        
+
+
                         <div className={styles['stock-article-text']}>
                             <p>
-                                In a report released today, from William Blair maintained a Hold rating on Re/Max
-                                Holdings (
-                                – ). The company’s shares closed today at $9.11. According to , Sheldon is a 3-star
-                                analyst
-                                with an average return of and a 47.37% success rate. Re/Max Holdings has an analyst
-                                consensus of Moderate Sell, with a price target consensus of $7.50.
-                                RMAX market cap is currently $319M and has a P/E ratio of -18.99. Based on the recent
-                                corporate insider activity of 67 insiders, corporate insider sentiment is positive on
-                                the
-                                stock. This means that over the past quarter there has been an increase of insiders
-                                buying
-                                their shares of RMAX in relation to earlier this year. Last month, Adam K Peterson, a
-                                Major
-                                Shareholder at RMAX bought 11,798.00 shares for a total of $113,732.72.
-                                I find myself hard-pressed, at times, to believe how often legacy automaker Ford can
-                                issue
-                                recalls on its products. In fact, another one just showed up, this time covering nearly
-                                a
-                                quarter of a million vehicles. And even investors look like they have had it with Ford;
-                                shares are down nearly 1.5% in Friday afternoon’s trading. This time, the recall targets
-                                Lincoln Aviators and Ford Explorers made between 2020 and 2021, the worst parts of the
-                                pandemic. That encompasses 240,510 such vehicles, and the focus is on seat belt anchor
-                                bolts. More specifically, the bolts may be improperly secured, which means that, in some
-                                cases, the whole housing could come undone, and that might be one of the last things
-                                anyone
-                                would want to see in a wreck. The fix is rather simple; once again, Ford owners will
-                                need to
-                                return to their dealership of choice, where dealers will inspect the bolts. Should they
-                                be
-                                found to be part of the problem, the bolts and related components will be replaced.
-                                There
-                                will likely be no charge for this, except for the time you lose for them.
-                                And good news for anyone paralyzed with indecision over the sheer cost of a car these
-                                days;
-                                the Ford Bronco’s new discount packages are still in play, and will continue to be so
-                                for
-                                the near term future, if nothing else. The 2024 Ford Bronco sport can still be had with
-                                a
-                                discount of $2,750 off, as well as “low-interest financing.” Plus, there are lease deals
-                                involved for anyone who would rather go that route. There is $2,000 in “conquest cash”
-                                still
-                                available for those who owned or leased a Jeep, as well as for those who owned or leased
-                                a
-                                General Motors vehicle. Neither trade-ins nor lease terminations are required, either.
-                                However, the 2025 Ford Bronco Sport does not have any incentives on it. At least, not
-                                yet;
-                                that could change the farther into the new market year we get. Turning to Wall Street,
-                                analysts have a Hold consensus rating on F stock based on four Buys, eight Holds and
-                                three
-                                Sells assigned in the past three months, as indicated by the graphic below. After a
-                                17.81%
-                                over the past year, the of $10.56 per share implies 13.98% upside potential.
+                                {display_article?display_article.article_text:""}
                             </p>
                         </div>
                     </div>
--- frontend/src/contexts/article_context.jsx
+++ frontend/src/contexts/article_context.jsx
@@ -2,11 +2,13 @@ import { createContext, useState, useContext, useEffect } from "react";
 import toast from "react-hot-toast";
 import { get, put, _delete, post } from "../api/api";
 import { UserContext } from "./user_context";
+import { data } from "react-router";
 
 export const ArticleContext = createContext(); 
 
 const ArticleContextProvider = ({ children }) => {
     const [articles, set_articles] = useState([]);
+    const [stock_data, set_stock_data] = useState([])
 
     // Use useEffect to log the updated articles state
     useEffect(() => {
@@ -22,11 +24,25 @@ const ArticleContextProvider = ({ children }) => {
         }
     };
 
+    const getStockData= async (stock_name)=>{
+        const res = await get(`get_ticker_data/?ticker=${stock_name}`)
+        if (res.data){
+            set_stock_data(res.data.Items); 
+        }
+        else{
+            toast.error("could not get stock data"); 
+        }
+    }
+
+    
+
     return (
         <ArticleContext.Provider
             value={{
                 getArticles,
                 articles,
+                getStockData, 
+                stock_data
             }}
         >
             {children}
